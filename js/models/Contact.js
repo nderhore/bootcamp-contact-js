d83378c1a03c0acf68edffdd98338151acf0084a
Nathan <nathan@derhore.fr>
2024-07-26 17:55:54 +0200

    feat(Contact):create contact class

---
+++ js/models/Contact.js
@@ -0,0 +1,17 @@
+/**
+ * Contact représente un contact disposant d'un :
+ * - nom
+ * - prenom
+ * - telephone
+ */
+
+export class Contact{
+
+    constructor(nom,prenom,telephone){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.telephone = telephone;
+    }
+}
+
+
