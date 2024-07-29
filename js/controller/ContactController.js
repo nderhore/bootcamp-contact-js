9fc3e19272a7fc419b35de9c398c67869a585fa7
Nathan <nathan@derhore.fr>
2024-07-29 20:37:06 +0200

    feat(contact): create and view contact html

--- js/controller/ContactController.js
+++ js/controller/ContactController.js
@@ -1,12 +1,48 @@
 import {Contact} from "../models/Contact.js";
 
 
-let listContacts = [];
+const listContacts = [];
 
-const contact = new Contact("dupont","anne","0789651232");
-const contactDeux = new Contact("deue","e","0789651231");
 
-for(let i = 0; i<100; i++){
-    listContacts.push(new Contact("dupont","anne","0789651232"));
+document.getElementById("creation-contact")
+    .addEventListener("submit", event => {
+    event.preventDefault();
+
+    const nom = document.getElementById("nom").value;
+    const prenom = document.getElementById("prenom").value;
+    const telephone = document.getElementById("telephone").value;
+
+    addContact(nom,prenom,telephone);
+    document.getElementById("creation-contact").reset();
+
+});
+
+function addContact(nom, prenom, telephone){
+    const contact = new Contact(nom,prenom,telephone);
+    listContacts.push(contact);
+    displayContact(contact)
 }
-console.log(listContacts);
+
+function displayContact(contact){
+
+    //recuperation du tbody de ma table ayant pour id 'list-contact'
+    const tableBody = document.querySelector("#list-contact tbody");
+
+    //creation d'une ligne de mon tableau
+    const newRow = document.createElement("tr");
+
+    //creation de mes données
+    const nomCell = document.createElement("td");
+    nomCell.innerText = contact.nom
+    newRow.appendChild(nomCell);
+
+    const prenomCell = document.createElement("td");
+    prenomCell.innerText = contact.prenom
+    newRow.appendChild(prenomCell);
+
+    const telephoneCell = document.createElement("td");
+    telephoneCell.innerText = contact.telephone
+    newRow.appendChild(telephoneCell);
+
+    tableBody.appendChild(newRow);
+}
